905b3a204bbcf76a07379bac7b6d033ad2992d41
Maruhan <didhddldlq@naver.com>
2016-01-12 09:45:10 +0900

    9:45AM cesium And Index Page modified by Maruhan

--- archistory/src/main/webapp/resources/Cesium/js/daumMap.js
+++ archistory/src/main/webapp/resources/Cesium/js/daumMap.js
@@ -17,15 +17,3 @@ var getParameter = function (param) {
 };
 
 
-
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-    mapOption = {
-        center: new daum.maps.LatLng(getParameter('lat'), getParameter('lng')), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    };
-
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-var map = new daum.maps.Map(mapContainer, mapOption);
-
-
-
