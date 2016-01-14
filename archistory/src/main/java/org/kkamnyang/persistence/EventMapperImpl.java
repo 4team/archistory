b5899a10fc68829e20dba92783aad891e1b82264
islejeon <islejeon@isle>
2016-01-14 15:03:52 +0900

    15:03

--- archistory/src/main/java/org/kkamnyang/persistence/EventMapperImpl.java
+++ archistory/src/main/java/org/kkamnyang/persistence/EventMapperImpl.java
@@ -1,10 +1,18 @@
 package org.kkamnyang.persistence;
 
+import java.util.List;
+
 import org.kkamnyang.domain.EventVO;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class EventMapperImpl extends AbstractCRUDMapper<EventVO,Integer> implements EventMapper {
+
+	@Override
+	public List<EventVO> elist(Integer routeno) {
+		// TODO Auto-generated method stub
+		return session.selectList(namespace+".elist",routeno);
+	}
 
 	
 }
