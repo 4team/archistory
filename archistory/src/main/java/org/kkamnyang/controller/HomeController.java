3c95ad139a9f55d76de54baad7d541f119ac5e71
Maruhan <didhddldlq@naver.com>
2016-01-20 12:30:02 +0900

    sss

--- archistory/src/main/java/org/kkamnyang/controller/HomeController.java
+++ archistory/src/main/java/org/kkamnyang/controller/HomeController.java
@@ -23,9 +23,8 @@ public class HomeController {
 	 * @throws Exception 
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model,HttpSecurity http) throws Exception {
+	public String home(Locale locale, Model model) throws Exception {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		 http.csrf().disable();
 		
 		return "home";
 	}
