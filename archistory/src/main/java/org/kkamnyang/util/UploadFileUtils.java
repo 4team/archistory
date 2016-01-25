e245d33727e6b77bb31bed773ff22a090d43867a
Maruhan <didhddldlq@naver.com>
2016-01-25 21:08:45 +0900

    zz

--- archistory/src/main/java/org/kkamnyang/util/UploadFileUtils.java
+++ archistory/src/main/java/org/kkamnyang/util/UploadFileUtils.java
@@ -138,7 +138,7 @@ return uploadedFileName;
 //        File.separator + 
 //        new DecimalFormat("00").format(cal.get(Calendar.DATE));
 //    
-//    makeDir(uploadPath, yearPath,monthPath,datePath);
+    makeDir(uploadPath, "picture");
 //    
 //    logger.info(datePath);
 //    
@@ -147,33 +147,33 @@ return uploadedFileName;
   }
   
   private static String calcMoviePath(String uploadPath){
-//	  int srcLenght = uploadPath.length();
-//	  
-//	  String meanPath = uploadPath.substring(14, srcLenght);
-//	  System.out.println("의미있는 경로 : " + meanPath);
-//	  
-//	  String category = uploadPath.substring(14,19);
-//	  
-//	  int meanLength = meanPath.length();
-//	  
-//	  String removeCate = meanPath.substring(6, meanLength);
-//	  
-//	  int endIndex = removeCate.lastIndexOf("\\");
-//	  
-//	  String route = removeCate.substring(0, endIndex);
-//	  System.out.println("루트 No : "+route);
-//	  int first = removeCate.indexOf("\\");
-//	  endIndex = removeCate.length();
-//	  
-//	  String filetype = removeCate.substring(first+1, endIndex);
-//	  System.out.println("FileTyep : "+ filetype);
-//	  
-//	  System.out.println("remove Category Path : " + removeCate);
-//	  
-//	  System.out.println(uploadPath);
-//	  System.out.println(category);
-//
-//	  makeDir(uploadPath,category,route,filetype);
+	  int srcLenght = uploadPath.length();
+	  
+	  String meanPath = uploadPath.substring(14, srcLenght);
+	  System.out.println("의미있는 경로 : " + meanPath);
+	  
+	  String category = uploadPath.substring(14,19);
+	  
+	  int meanLength = meanPath.length();
+	  
+	  String removeCate = meanPath.substring(6, meanLength);
+	  
+	  int endIndex = removeCate.lastIndexOf("\\");
+	  
+	  String route = removeCate.substring(0, endIndex);
+	  System.out.println("루트 No : "+route);
+	  int first = removeCate.indexOf("\\");
+	  endIndex = removeCate.length();
+	  
+	  String filetype = removeCate.substring(first+1, endIndex);
+	  System.out.println("FileTyep : "+ filetype);
+	  
+	  System.out.println("remove Category Path : " + removeCate);
+	  
+	  System.out.println(uploadPath);
+	  System.out.println(category);
+
+	  makeDir(uploadPath,category,route,filetype);
 	  return "\\movie";
 	  
   }
