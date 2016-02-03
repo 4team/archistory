f361291956cc8268702417931b4d0a72faf03353
islejeon <islejeon@isle>
2016-02-03 15:39:18 +0900

    1

--- archistory/src/main/java/org/kkamnyang/controller/AdminController.java
+++ archistory/src/main/java/org/kkamnyang/controller/AdminController.java
@@ -245,7 +245,7 @@ public class AdminController {
 		
 		model.addAttribute("savedName",savedName);
 		
-		return "redirect:" + "admin/login_success";
+		return "/admin/login_success";
 	}
 	
 	private String uploadFile(String originalName, byte[] fileData) throws Exception{
