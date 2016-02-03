62114b883e3a8618d10a27bba5361393a88ef667
islejeon <islejeon@isle>
2016-02-03 16:23:36 +0900

    1

--- archistory/src/main/java/org/kkamnyang/filter/SimpleCORSFilter.java
+++ archistory/src/main/java/org/kkamnyang/filter/SimpleCORSFilter.java
@@ -22,7 +22,7 @@ public class SimpleCORSFilter implements Filter {
                 response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
                 response.addHeader("Access-Control-Allow-Headers", "Authorization");
                 response.addHeader("Access-Control-Max-Age", "1728000");
-                response.setHeader("X-Frame-Options", "ALLOW-FROM http://14.32.66.127:4000");
+               
             }
             filterChain.doFilter(request, response);
         }
