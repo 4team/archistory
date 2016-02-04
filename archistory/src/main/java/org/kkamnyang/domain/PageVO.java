fd9541c4c08427d03e8c0e6739697e76f5e0d55a
Maruhan <didhddldlq@naver.com>
2016-02-04 10:57:14 +0900

    z

---
+++ archistory/src/main/java/org/kkamnyang/domain/PageVO.java
@@ -0,0 +1,31 @@
+package org.kkamnyang.domain;
+
+public class PageVO {
+	
+	private java.lang.Integer routeno;
+	private java.lang.Integer memberno;
+	private java.lang.Integer page;
+	
+	public java.lang.Integer getRouteno() {
+		return routeno;
+	}
+	public void setRouteno(java.lang.Integer routeno) {
+		this.routeno = routeno;
+	}
+	public java.lang.Integer getMemberno() {
+		return memberno;
+	}
+	public void setMemberno(java.lang.Integer memberno) {
+		this.memberno = memberno;
+	}
+	public java.lang.Integer getPage() {
+		return page;
+	}
+	public void setPage(java.lang.Integer page) {
+		this.page = page;
+	}
+	
+	
+	
+	
+}
